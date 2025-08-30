836af55e98910c65627e33e0b9f04638652c077f
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-30 22:45:44 +0430

    pdf requirement added

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import json
 import os
 import uuid
 from datetime import datetime
+
+# for pdf
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
