61ba9d6ce4156689a802dbe37dbbd5f968da769d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-30 22:47:39 +0430

    lead_contact function

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ from reportlab.lib.styles import getSampleStyleSheet
 
 # json file name
 DATA_FILE = "contacts.json"
+
+# json data section
+
+# load_contacts from json file
+def load_contacts():
+    if not os.path.exists(DATA_FILE):
+        return []
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return []
